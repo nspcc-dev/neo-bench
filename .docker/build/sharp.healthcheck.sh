9b5aa3d9b4d0520987e7431c1637e8ffc56bb563
Roman Khimov <roman@nspcc.ru>
2020-07-11 20:19:43 +0300

    sharp.healthcheck: fix address
    0.0.0.0 is the bind address, healthchecker should just connect to the
    localhost.

--- .docker/build/sharp.healthcheck.sh
+++ .docker/build/sharp.healthcheck.sh
@@ -2,7 +2,7 @@
 
 maxDelayBlocks=10
 export port=`jq -r '.ApplicationConfiguration.RPC.Port' < /neo-cli/config.json`
-export host=`jq -r '.ApplicationConfiguration.RPC.BindAddress' < /neo-cli/config.json`
+export host=127.0.0.1
 export addr=${host}:${port}
 echo curl -s -X POST http://${addr} -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "id": 5, "method": "getblockcount", "params": [] }'
 curBlock=`curl -s -X POST http://${addr} -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "id": 5, "method": "getblockcount", "params": [] }'| jq '.result'`
