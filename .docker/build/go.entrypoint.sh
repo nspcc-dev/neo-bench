aa309ccd2bde1107df14d6baf2388e6dbf36c5d5
Evgenii Stratonikov <evgeniy@nspcc.ru>
2021-08-24 13:41:39 +0300

    docker: add network delays/losses
    Everything is done via `--tc` flag or `NEOBENCH_TC` environment variable
    which contains arguments passed to `tc` invocation inside the container.
    Example:
    `./runner.sh -w 10 -z 5m --tc "delay 100ms loss random 0.1% rate 1Mbit"`
    
    For this to work container needs CAP_NET_ADMIN capability.
    
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- .docker/build/go.entrypoint.sh
+++ .docker/build/go.entrypoint.sh
@@ -6,6 +6,16 @@ if [ -z "$ACC" ]; then
   ACC=single.acc
 fi
 
+if [ -n "$NEOBENCH_TC" ]; then
+  # shellcheck disable=SC2086 # Intended splitting of $NEOBENCH_TC (may be "100ms 10ms distribution normal")
+  if tc qdisc add dev eth0 root netem $NEOBENCH_TC; then
+    echo "Set qdisc to netem $NEOBENCH_TC"
+  else
+    echo "Can't set qdisc to netem $NEOBENCH_TC"
+    exit 1
+  fi
+fi
+
 case $@ in
 	"node"*)
 	echo "=> Try to restore blocks before running node"
