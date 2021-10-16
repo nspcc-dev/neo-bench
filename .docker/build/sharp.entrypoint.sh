d8a63140a80ea4c4147c9f300eec185b9456c6d5
Evgenii Stratonikov <evgeniy@nspcc.ru>
2021-10-16 15:11:35 +0300

    *: run shell scripts through `shfmt`
    Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

--- .docker/build/sharp.entrypoint.sh
+++ .docker/build/sharp.entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [ -z "$BIN" ]; then
-  BIN=/neo-cli/neo-cli
+	BIN=/neo-cli/neo-cli
 fi
 
 if [ -n "$NEOBENCH_TC" ]; then
-  # shellcheck disable=SC2086 # Intended splitting of $NEOBENCH_TC (may be "100ms 10ms distribution normal")
-  if tc qdisc add dev eth0 root netem $NEOBENCH_TC; then
-    echo "Set qdisc to netem $NEOBENCH_TC"
-  else
-    echo "Can't set qdisc to netem $NEOBENCH_TC"
-    exit 1
-  fi
+	# shellcheck disable=SC2086 # Intended splitting of $NEOBENCH_TC (may be "100ms 10ms distribution normal")
+	if tc qdisc add dev eth0 root netem $NEOBENCH_TC; then
+		echo "Set qdisc to netem $NEOBENCH_TC"
+	else
+		echo "Can't set qdisc to netem $NEOBENCH_TC"
+		exit 1
+	fi
 fi
 
 ${BIN} "$@"
--- .docker/build/sharp.healthcheck.sh
+++ .docker/build/sharp.healthcheck.sh
@@ -6,34 +6,30 @@ export port=$(jq -r '.PluginConfiguration.Servers[0].Port' </neo-cli/Plugins/Rpc
 export host=127.0.0.1
 export addr=${host}:${port}
 echo curl -s -X POST "http://$addr" -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "id": 5, "method": "getblockcount", "params": [] }'
-curBlock=$(curl -s -X POST "http://$addr" -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "id": 5, "method": "getblockcount", "params": [] }'| jq '.result')
+curBlock=$(curl -s -X POST "http://$addr" -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "id": 5, "method": "getblockcount", "params": [] }' | jq '.result')
 
-if [ "$curBlock" == "" ]
-then
-    curl -X POST "http://$addr" -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "id": 5, "method": "getblockcount", "params": [] }'
-    echo "${addr} => '${host} : ${port}'"
-    echo "NODE IS DOWN"
-    exit 1
+if [ "$curBlock" == "" ]; then
+	curl -X POST "http://$addr" -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "id": 5, "method": "getblockcount", "params": [] }'
+	echo "${addr} => '${host} : ${port}'"
+	echo "NODE IS DOWN"
+	exit 1
 fi
 
-nodes=$(jq -r .ProtocolConfiguration.SeedList[] < /neo-cli/config.json | sed 's/:20/:30/')
+nodes=$(jq -r .ProtocolConfiguration.SeedList[] </neo-cli/config.json | sed 's/:20/:30/')
 
-for node in "${nodes[@]}"
-do
-    block=$(curl -s -X POST "http://$node" -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "id": 5, "method": "getblockcount", "params": [] }'| jq '.result')
+for node in "${nodes[@]}"; do
+	block=$(curl -s -X POST "http://$node" -H 'Content-Type: application/json' -d '{ "jsonrpc": "2.0", "id": 5, "method": "getblockcount", "params": [] }' | jq '.result')
 
-    if [ "$block" == "" ]
-    then
-        block=0
-    fi
+	if [ "$block" == "" ]; then
+		block=0
+	fi
 
-    syncDelay=$(( block - curBlock ))
+	syncDelay=$((block - curBlock))
 
-    if [ "$syncDelay" -gt "$maxDelayBlocks" ]
-    then
-        echo "NODE OUT OF SYNC"
-        exit 2
-    fi
+	if [ "$syncDelay" -gt "$maxDelayBlocks" ]; then
+		echo "NODE OUT OF SYNC"
+		exit 2
+	fi
 done
 
 echo "ALL OK - NODE SYNCED"
